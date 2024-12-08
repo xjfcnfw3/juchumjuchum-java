05d74c45bc4dc3a0a87a7871b599a5857d72bd7d
kimminsu <xjfcnfw3@naver.com>
2024-12-08 21:05:06 +0900

    🚚 chore: 프로젝트 초기화

---
+++ src/main/java/com/ant/juchumjuchum/JuchumjuchumApplication.java
@@ -0,0 +1,13 @@
+package com.ant.juchumjuchum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuchumjuchumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuchumjuchumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ant/juchumjuchum/JuchumjuchumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ant.juchumjuchum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuchumjuchumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
