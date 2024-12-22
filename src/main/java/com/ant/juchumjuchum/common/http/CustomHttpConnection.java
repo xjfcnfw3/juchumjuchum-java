615114bdd5977d5ae96caf15e7e6119363cc375a
kimminsu <xjfcnfw3@naver.com>
2024-12-22 21:29:36 +0900

    ✨ feat: post 요청 기능 구현

--- src/main/java/com/ant/juchumjuchum/common/http/CustomHttpConnection.java
+++ src/main/java/com/ant/juchumjuchum/common/http/CustomHttpConnection.java
@@ -1,11 +1,17 @@
 package com.ant.juchumjuchum.common.http;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Map;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +23,22 @@ public class CustomHttpConnection {
         return connection.getInputStream();
     }
 
+    public Object postRequest(String url, Map<String, String> body, Class<?> clazz)
+            throws IOException, URISyntaxException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        ObjectMapper objectMapper = new ObjectMapper();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(url))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(body)))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (response.statusCode() != HttpURLConnection.HTTP_OK) {
+            throw new IOException("Failed to post request to " + url);
+        }
+        return objectMapper.registerModule(new JavaTimeModule()).readValue(response.body(), clazz);
+    }
 
     private void validateStatus(String downloadUrl, HttpURLConnection connection) throws IOException {
         if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
