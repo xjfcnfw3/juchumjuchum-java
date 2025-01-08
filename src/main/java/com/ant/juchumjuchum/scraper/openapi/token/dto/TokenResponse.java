a22d5420ce0f9c1e6c9a51d2a8e99ba8886b4946
kimminsu <xjfcnfw3@naver.com>
2025-01-08 21:33:48 +0900

    🐛 fix: 생성자로 인한 토큰 역직렬화 문제 해결

--- src/main/java/com/ant/juchumjuchum/scraper/openapi/token/dto/TokenResponse.java
+++ src/main/java/com/ant/juchumjuchum/scraper/openapi/token/dto/TokenResponse.java
@@ -6,10 +6,12 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class TokenResponse {
 
     @JsonProperty("access_token")
