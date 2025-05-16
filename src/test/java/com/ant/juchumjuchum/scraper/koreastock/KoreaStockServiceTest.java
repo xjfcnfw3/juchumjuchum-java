838530b54686f70d281ec70195e4671abe5561a9
kimminsu <xjfcnfw3@naver.com>
2025-05-16 18:49:35 +0900

    ✅ test: 테스트 입력 전용 문자열을 파일로부터 가져오도록 변경

--- src/test/java/com/ant/juchumjuchum/scraper/koreastock/KoreaStockServiceTest.java
+++ src/test/java/com/ant/juchumjuchum/scraper/koreastock/KoreaStockServiceTest.java
@@ -13,6 +13,9 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import org.junit.jupiter.api.DisplayName;
@@ -24,14 +27,6 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class KoreaStockServiceTest {
-
-    String kosdaq =
-            "900110   HK0000057197이스트아시아홀딩스                      FS 000000000000NNN NNNNNNNN0NNNNNNNN0000000470000100001NNN00NNN000000100N0900000025938880000000000002010042300000000064265000000000000090451845512       0 NN0000007410000000810000000830005200000000220241231000000302   NNN\n"
-                    + "900270   HK0000214814헝셩그룹                                FS 000000000000NNN NNNNNNNN0NNNNNNNN0000002400000100001NNN00NNN000000100N0900000113589040000000000002016081800000000017628200000000000055610722012       0 NN0000011710000000740000000490002300000000120241231000000423   NNN";
-
-    String kospi =
-            "F70100009KR5701000097한투삼성그룹성장테마1호(A)              BC 000000000000NN 0NNN NN    N  0  N     0000010080000100000NNN00NNN000000100N09000000000000000000000100020240809000000000017771000000000017771907000         0 NNN00000000000000000000000000000000000000.00        000000179   NNN\n"
-                    + "F70100010KR5701000105한투삼성그룹성장테마1호(A-e)            BC 000000000000NN 0NNN NN    N  0  N     0000010090000100000NNN00NNN000000100N09000000000000000000000100020240809000000000011074000000000011074770000         0 NNN00000000000000000000000000000000000000.00        000000111   NNN\n";
 
     @InjectMocks
     KoreaStockService koreaStockService;
@@ -47,7 +42,7 @@ class KoreaStockServiceTest {
     void downloadKosdaq() throws IOException, ExecutionException, InterruptedException {
 
         File mstFile = new File("kosdaq_code.mst");
-        writeFile(mstFile, kosdaq);
+        writeFile(mstFile, "test-kosdaq-inputs.txt");
 
         File fakeZipFile = new File("kosdaq_code.mst.zip");
         fakeZipFile.createNewFile();
@@ -68,7 +63,7 @@ class KoreaStockServiceTest {
     void downloadKospi() throws IOException, ExecutionException, InterruptedException {
 
         File mstFile = new File("kospi_code.mst");
-        writeFile(mstFile, kospi);
+        writeFile(mstFile, "test-kospi-inputs.txt");
         File fakeZipFile = new File("kospi_code.mst.zip");
         fakeZipFile.createNewFile();
         when(koreaStockDownloadService.downloadStockInfo("kospi_code"))
@@ -94,10 +89,13 @@ class KoreaStockServiceTest {
         assertThat(result).isEqualTo(1);
     }
 
-    private void writeFile(File mstFile, String kospi) {
+    private void writeFile(File mstFile, String filename) throws IOException {
+        List<String> inputs = Files.readAllLines(Paths.get("src/test/resources/" + filename));
         try (BufferedWriter writer = new BufferedWriter(
                 new OutputStreamWriter(new FileOutputStream(mstFile), "ms949"))) {
-            writer.write(kospi);
+            for (String line : inputs) {
+                writer.write(line);
+            }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
